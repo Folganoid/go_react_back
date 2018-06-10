7764318159ba094357ea20ad88de48139ab3969e
Andrey <folganoid@yahoo.com>
2018-06-10 21:21:38 +0300

    (fnc) gorm

--- main.go
+++ main.go
@@ -1,19 +1,58 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
+)
+
+type User struct {
+	Id    int64
+	Login string
+	Pass  string
+	Email string
+	Token string
+}
 
 func main() {
-    app := iris.New()
-    app.RegisterView(iris.HTML("./views", ".html"))
-    app.Get("/", func(ctx iris.Context) {
-        ctx.ViewData("message", "Hello world!")
-        ctx.View("hello.html")
-    })
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	app.Use(recover.New())
+	app.Use(logger.New())
 
-    app.Get("/user/{id:long}", func(ctx iris.Context) {
-        userID, _ := ctx.Params().GetInt64("id")
-        ctx.Writef("User ID: %d", userID)
-    })
+	app.RegisterView(iris.HTML("./views", ".html"))
+	app.Get("/", func(ctx iris.Context) {
+		ctx.ViewData("message", "Hello world!")
+		ctx.View("hello.html")
+	})
 
-    app.Run(iris.Addr(":8080"))
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.WriteString("pong")
+	})
+
+	app.Get("/hello", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "Hello Iris!"})
+	})
+
+	app.Get("/test", func(ctx iris.Context) {
+
+		db, err := gorm.Open("mysql", "fg:5619@/go?charset=utf8")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer db.Close()
+
+		var user User
+		db.First(&user, 1)
+
+		fmt.Println(user)
+
+	})
+
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
